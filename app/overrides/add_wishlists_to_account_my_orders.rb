149f37c07dee47d00d0fd156aba2ae6a9c1dbdef
Thomas Boltze <thomas.boltze@gmail.com>
2012-11-02 13:27:26 +0800

    Disabled wishlists on my-account

--- app/overrides/add_wishlists_to_account_my_orders.rb
+++ app/overrides/add_wishlists_to_account_my_orders.rb
@@ -1,5 +1,7 @@
+# Disabled for now, can't figure out what they want with it.
 Deface::Override.new(:virtual_path => "spree/users/show",
                      :name => "add_wishlists_to_account_my_orders",
                      :insert_after => "[data-hook='account_my_orders']",
                      :partial => "spree/users/wishlists",
-                     :disabled => false)
+                     :disabled => true)
+
